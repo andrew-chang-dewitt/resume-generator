d0817f7e3bb6657415a3fa05fcf4a394a8704e5c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-09-23 22:31:49 -0500

    Start scaffolding out basic cli.

--- TODO.md
+++ TODO.md
@@ -1,9 +1,8 @@
-TODO:
-===
+# TODO:
 
-- [ ] GitHub repo!
-- [ ] Fix env file imports in docker-compose file and main.rs
+- [x] GitHub repo!
+- [x] Fix env file imports in docker-compose file and main.rs
 - [ ] Turn db up into script to remember env file tags? — not dev script though, this is more of an env thing
-- [ ] Configure clap
+- [x] Configure clap
 - [ ] Write mvp cli args, basic inputs and expected outputs
 - [ ] Study sqlx todo example with testing to see how to start testing functions before writing any
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
-use std::env;
-
+use async_trait::async_trait;
+use clap::{Parser, Subcommand};
 use sqlx::PgPool;
+
+use std::{env, io::Write};
+
+#[derive(Parser)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Option<Command>,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    Add { description: String },
+    Del { id: i64 },
+}
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> anyhow::Result<()> {
     // get env vars
     dotenvy::dotenv().ok();
-    // create connection pool
+
+    // get cli args
+    let args = Args::parse();
+
+    // create db connection
     let user = &env::var("PGUSER")?;
     let pass = &env::var("PGPASS")?;
     let host = &env::var("PGHOST")?;
@@ -14,14 +32,46 @@ async fn main() -> anyhow::Result<()> {
     let name = &env::var("DBNAME")?;
     let url = format!("postgres://{user}:{pass}@{host}:{port}/{name}");
     let pool = PgPool::connect(&url).await?;
+    // and create data model w/ db connection
+    let resume_model = ResumeModel::new(pool);
 
-    // let's just start by printing some data:
-    let row: (i64,) = sqlx::query_as("SELECT $1")
-        .bind(150_i64)
-        .fetch_one(&pool)
-        .await?;
+    // setup stdio write stream
+    let mut writer = std::io::stdout();
 
-    println!("{row:#?}");
+    handle_command(args, model, &mut writer).await
+}
+
+async fn handle_command(
+    args: Args,
+    model: impl ResumeModel,
+    writer: &mut impl Write,
+) -> anyhow::Result<()> {
+    match args.cmd {
+        Some(Command::Add { description }) => {
+            writeln!(writer, "Adding data w/ description '{}'", &description)?;
+            let id = model.add_data(description).await?;
+            writeln!(writer, "Added data with id {id}")?;
+        }
+        Some(Command::Del { id }) => {
+            writeln!(writer, "Deleting data w/ id '{}'", &id)?;
+            if model.del_data(id).await? {
+                writeln!(writer, "Deleted data with id {id}")?;
+            } else {
+                writeln!(writer, "Invalid id {id}")?;
+            }
+        }
+        None => {
+            writeln!(writer, "Printing all data")?;
+            model.lst_data().await?;
+        }
+    }
 
     Ok(())
+}
+
+#[async_trait]
+trait ResumeModel {
+    async fn add_data(&self, description: String) -> anyhow::Result<i64>;
+    async fn del_data(&self, id: i64) -> anyhow::Result<bool>;
+    async fn lst_data(&self) -> anyhow::Result<()>;
 }
