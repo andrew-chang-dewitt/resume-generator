99e967cd6454cf2b832f0552af5c187d7e133b1c
Andrew Chang-DeWitt <andrew@andrew-chang-dewitt.dev>
2024-09-23 22:04:09 -0500

    Fix .env files & vars.

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,13 @@ async fn main() -> anyhow::Result<()> {
     // get env vars
     dotenvy::dotenv().ok();
     // create connection pool
-    let pool = PgPool::connect(&env::var("DATABASE_URL")?).await?;
+    let user = &env::var("PGUSER")?;
+    let pass = &env::var("PGPASS")?;
+    let host = &env::var("PGHOST")?;
+    let port = &env::var("PGPORT")?;
+    let name = &env::var("DBNAME")?;
+    let url = format!("postgres://{user}:{pass}@{host}:{port}/{name}");
+    let pool = PgPool::connect(&url).await?;
 
     // let's just start by printing some data:
     let row: (i64,) = sqlx::query_as("SELECT $1")
